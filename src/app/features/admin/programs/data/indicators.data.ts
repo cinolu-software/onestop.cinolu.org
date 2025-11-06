7a0a5d09c12b7618ffa266c54544512c7e7db913
Wilfried Musanzi <musanziw.m@gmail.com>
2025-11-06 16:46:23 +0200

    fix: update INDICATORS_CATEGORIES to correct category names

--- src/app/features/admin/programs/data/indicators.data.ts
+++ src/app/features/admin/programs/data/indicators.data.ts
@@ -1,1 +1,1 @@
-export const INDICATORS_CATEGORIES: string[] = ['Entrants', 'Produits(Outputs)', 'Résultats(Outcomes)', 'Impact'];
+export const INDICATORS_CATEGORIES: string[] = ['Ressources', 'Produits', 'Activités', 'Résultats', 'Impact'];
