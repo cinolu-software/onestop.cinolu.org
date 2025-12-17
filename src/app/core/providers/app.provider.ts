5393dc97d6fea86fa7c9e422d9ad636165602e00
Wilfried Musanzi <musanziw.m@gmail.com>
2025-12-17 15:05:33 +0200

    refactor(auth): update profile retrieval to use HttpClient for improved error handling and user state management

--- src/app/core/providers/app.provider.ts
+++ src/app/core/providers/app.provider.ts
@@ -1,14 +1,26 @@
 import { EnvironmentProviders, inject, provideAppInitializer, Provider } from '@angular/core';
-import { APP_CONFIG } from '../../shared/services/config/config.constants';
 import { appConfig } from '../../app.config';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, of } from 'rxjs';
 import { AuthStore } from '../auth/auth.store';
+import { IUser } from '@shared/models';
+import { APP_CONFIG } from '@shared/services/config';
 
 export const provideApp = (): EnvironmentProviders[] => {
   const providers: Provider | EnvironmentProviders = [
     { provide: APP_CONFIG, useValue: appConfig || {} },
     provideAppInitializer(() => {
       const authStore = inject(AuthStore);
-      authStore.getProfile();
+      const http = inject(HttpClient);
+      return http.get<{ data: IUser }>('auth/profile').pipe(
+        map(({ data }) => {
+          authStore.setUser(data);
+        }),
+        catchError(() => {
+          authStore.setUser(null);
+          return of(null);
+        })
+      );
     })
   ];
   return providers;
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   apiUrl: 'http://localhost:8000/',
-  appUrl: 'http://localhost:4200/',
+  appUrl: 'http://localhost:4200/'
 };
